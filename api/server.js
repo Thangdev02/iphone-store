50859b615d51b6728452dd6bb004ac1b0aa2ff8f
Le Quoc Thang <thanglq@Nghiems-MacBook.local>
2024-11-20 20:21:20 +0700

    Updated server and vercel.json

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 
 module.exports = (req, res) => {
   const server = jsonServer.create();
-  const router = jsonServer.router('database.json'); // Ensure the path to the database is correct
+  const router = jsonServer.router('./database.json'); // Make sure path is correct
   const middlewares = jsonServer.defaults();
 
   server.use(middlewares);
